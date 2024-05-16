95a8c7ed94970f018b5439300d04313b8ca0ceb2
mandibchaulagain <mandibchaulagain8@gmail.com>
2024-05-16 18:29:48 +0545

    generation of poission variable program

---
+++ Generation_of_Poission_Variate.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cmath>
+#include<string>
+#include <algorithm>
+using namespace std;
+int main() {
+    double e = M_E; // Euler's number
+    float component = -0.2;
+    double New_e = pow(e,component);
+    cout<<New_e<<endl;//0.8187
+    int n = 0;
+    double p = 1;
+    double randomVarArray[] = {0.4357,0.4146,0.8353,0.9952,0.8004};
+    string acceptOrNot;
+    int lengthofArray = sizeof(randomVarArray)/sizeof(randomVarArray[0]);
+    cout<<"Length of array = "<<lengthofArray<<endl;
+    int Result[lengthofArray-1];
+    for(int i=0;i<lengthofArray;i++){
+    	if(i!=0){
+    		if(p>=New_e){
+				n+=1;
+						
+			}else{
+				n=0;p=1;
+			}
+		}
+		Result[i] = n;
+		p = p*randomVarArray[i];
+		if(p>=New_e){
+			acceptOrNot = "Rejected";
+		}
+		else{
+			acceptOrNot = "Accepted";
+		}
+		cout<<"n is: "<<n<<" Rn+1: "<<randomVarArray[i]<<" P is: "<<p<<"n is "<<acceptOrNot<<endl;
+	}
+	int* maxElement = max_element(Result, Result + lengthofArray);
+	cout<<"Result: n = "<<*maxElement<<endl;
+    return 0;
+}
+
